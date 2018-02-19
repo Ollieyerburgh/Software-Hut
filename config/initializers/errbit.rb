f9cb61d4c33efaa4a9410a5746bfc00a04c3687d
Aca15i@shefuniad.shef.ac.uk <aca15i@ltsp938.shef.ac.uk>
2018-02-19 12:03:50 +0000

    Changes

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,5 @@
 Airbrake.configure do |config|
-  config.api_key = 'c560d240a4604565532886e262af6309'
+  config.api_key = 'REPLACE_ME'
   config.host    = 'errbit.hut.shefcompsci.org.uk'
   config.port    = 443
   config.secure  = config.port == 443
