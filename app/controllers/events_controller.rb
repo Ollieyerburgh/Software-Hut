f5a244c5a6fede75ec5c6ec23e29538622e115e1
owardell-yerburgh1@sheffield.ac.uk <ollie@Ollies-MacBook-Pro.local>
2018-03-07 11:34:12 +0000

    Added abilities. Guests cannot see saved events, but can create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,9 @@
 class EventsController < ApplicationController
+
+
   before_action :set_event, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:new]
+  authorize_resource
 
   # GET /events
   def index
@@ -14,6 +17,7 @@ class EventsController < ApplicationController
 
   # GET /events/new
   def new
+    @current_nav_identifier = :new_event
     @event = Event.new
   end
 
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,13 +3,16 @@ class Ability
 
   def initialize(user)
     # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
+
+   unless user
+     can :create, Event
+   else
+     can :manage, Event
+     if user.is_admin?
+       can :manage, :all
+     end
+
+   end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
