12c259c4986231cb9df4e423a7d019ecd2830fa7
Isaac <isaacjoyous@outlook.com>
2018-03-23 11:30:13 +0000

    Search works for both resource + activity

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,6 +4,8 @@ class SearchesController < ApplicationController
   def show
     @search=params[:search]
     @activities = Activity.where("lower(description) LIKE ?", "%#{@search.downcase}%" ).paginate(page: params[:page], per_page: 10)
+    @resources = Resource.where("lower(description) LIKE ?", "%#{@search.downcase}%" ).paginate(page: params[:page], per_page: 10)
+    @results_length = @activities.size + @resources.size
   end
 
   # GET /searches/new
