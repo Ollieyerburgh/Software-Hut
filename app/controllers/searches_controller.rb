50326095007e6724edaee3d0f12e3e923a3e9998
Isaac <isaacjoyous@outlook.com>
2018-04-15 11:59:53 +0100

    Added javascript to get chosen subject on search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,6 +6,11 @@ class SearchesController < ApplicationController
   # GET /searches/1
   def show
     @search=params[:search]
+    p params
+    @subject=params[:subject]
+    if @subject=="Subject"
+      puts "Subject not supplied"
+    end
     @activities = Activity.where("lower(description) LIKE ? OR lower(title) LIKE ? OR lower(address) LIKE ?", "%#{@search.downcase}%","%#{@search.downcase}%" ,"%#{@search.downcase}%").paginate(page: params[:page], per_page: 10)
     @resources = Resource.where("lower(description) LIKE ? OR lower(link) LIKE ? OR lower(title) LIKE ? OR lower(email) LIKE ?", "%#{@search.downcase}%","%#{@search.downcase}%", "%#{@search.downcase}%" ,"%#{@search.downcase}%").paginate(page: params[:page], per_page: 10)
     @results_length = @activities.size + @resources.size
