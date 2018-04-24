797efe94f288ff1c630b6f53d343e6a553888dcd
owardell-yerburgh1@sheffield.ac.uk <ollie@Ollies-MacBook-Pro.local>
2018-04-24 16:21:40 +0100

    fixing errors

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -10,11 +10,8 @@
 
 class Delivery < ApplicationRecord
   has_and_belongs_to_many :preferences
-<<<<<<< HEAD
   validates :method, presence: false
-=======
+
   has_and_belongs_to_many :activities
 
-  validates :delivery_id, presence: false
->>>>>>> e837aec1345a73afbc528b5e62ce4274116ba76a
 end
