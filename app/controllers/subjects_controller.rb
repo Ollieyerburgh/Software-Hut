2d40b24f5d7ea51c212e3bd327f2478192b41e33
Cesim <fccivisilli1@sheffield.ac.uk>
2018-05-01 14:35:23 +0100

    Admin edit preference options refined

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -22,7 +22,7 @@ class SubjectsController < ApplicationController
     @subject = Subject.new(subject_params)
 
     if @subject.save
-      redirect_to subjects_path, notice: 'Subject was created.'
+      redirect_to :admin_index, notice: 'Subject was created.'
     else
       render :new
     end
@@ -44,7 +44,7 @@ class SubjectsController < ApplicationController
   # DELETE /subjects/1
   def destroy
     @subject.destroy
-    redirect_to subjects_url, notice: 'Subject was successfully destroyed.'
+    redirect_to admin_preference_path, method: :get, notice: 'Subject was successfully destroyed.'
   end
 
   private
