88a2b192ed0c3e76d332f827491d970f0f0debf0
Cesim <fccivisilli1@sheffield.ac.uk>
2018-05-01 16:53:54 +0100

    Activity styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,8 @@ User.where(email: 'Anon@anon.com').first_or_create(password: 'password', passwor
 
 user = User.find_by_email("fake@sheffield.ac.uk")
 10.times do
-  user.activities.create(title: Faker::Name.title, description: Faker::Company.bs, start_date: '01/13/2019', end_date: '02/13/2019', deadline: '01/13/2019', postcode: Faker::Address.postcode, link: Faker::Internet.url, email: Faker::Internet.email, address: Faker::Address.street_address, status: 'rejected')
+  user.activities.create(title: Faker::Name.title, description: Faker::Company.bs, start_date: '01/13/2019', end_date: '02/13/2019', deadline: '01/13/2019', postcode: Faker::Address.postcode,
+    link: Faker::Internet.url, email: Faker::Internet.email, address: Faker::Address.street_address, status: 'approved', subject_ids: [1,2], theme_ids: [3,4], delivery_ids: [1], age_ids: [1])
   user.resources.create(title: Faker::Name.unique.name, link: Faker::Internet.url, description: Faker::Company.bs, email: Faker::Internet.email, status: 'pending')
 end
 
