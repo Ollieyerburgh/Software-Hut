b8f8ec986a8b11fda8f84692dcdbd06296112c50
owardell-yerburgh1@sheffield.ac.uk <ollie@dyn224091.shef.ac.uk>
2018-05-03 15:41:42 +0100

    changed styling around my activities, shows like now

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,12 +30,12 @@ Admin.where(email: 'admin1@sheffield.ac.uk').first_or_create(password:'password'
 
 user = User.find_by_email("fake@sheffield.ac.uk")
 
-user.activities.create(title: Faker::Name.title, description: Faker::Company.bs, start_date: '01/13/2019', end_date: '02/13/2019', deadline: '01/13/2019', postcode: Faker::Address.postcode, link: Faker::Internet.url, email: Faker::Internet.email, address: Faker::Address.street_address, status: 'approved', theme_ids: [1,2], subject_ids: [1,5], delivery_ids: [1], age_ids: [1])
-user.activities.create(title: Faker::Name.title, description: Faker::Company.bs, start_date: '01/13/2019', end_date: '02/13/2019', deadline: '01/13/2019', postcode: Faker::Address.postcode, link: Faker::Internet.url, email: Faker::Internet.email, address: Faker::Address.street_address, status: 'approved', theme_ids: [1,3], subject_ids: [3,5], delivery_ids: [2,1], age_ids: [1])
-user.activities.create(title: Faker::Name.title, description: Faker::Company.bs, start_date: '01/13/2019', end_date: '02/13/2019', deadline: '01/13/2019', postcode: Faker::Address.postcode, link: Faker::Internet.url, email: Faker::Internet.email, address: Faker::Address.street_address, status: 'approved', theme_ids: [2,5], subject_ids: [1], delivery_ids: [], age_ids: [])
+#user.activities.create(title: Faker::Name.title, description: Faker::Company.bs, start_date: '01/13/2019', end_date: '02/13/2019', deadline: '01/13/2019', postcode: Faker::Address.postcode, link: Faker::Internet.url, email: Faker::Internet.email, address: Faker::Address.street_address, status: 'approved', theme_ids: [1,2], subject_ids: [1,5], delivery_ids: [1], age_ids: [1])
+#user.activities.create(title: Faker::Name.title, description: Faker::Company.bs, start_date: '01/13/2019', end_date: '02/13/2019', deadline: '01/13/2019', postcode: Faker::Address.postcode, link: Faker::Internet.url, email: Faker::Internet.email, address: Faker::Address.street_address, status: 'approved', theme_ids: [1,3], subject_ids: [3,5], delivery_ids: [2,1], age_ids: [1])
+#user.activities.create(title: Faker::Name.title, description: Faker::Company.bs, start_date: '01/13/2019', end_date: '02/13/2019', deadline: '01/13/2019', postcode: Faker::Address.postcode, link: Faker::Internet.url, email: Faker::Internet.email, address: Faker::Address.street_address, status: 'approved', theme_ids: [2,5], subject_ids: [1], delivery_ids: [], age_ids: [])
 
-10.times do
-  user.activities.create(title: Faker::Name.title, description: Faker::Company.bs, start_date: '01/13/2019', end_date: '02/13/2019', deadline: '01/13/2019', postcode: Faker::Address.postcode,
-    link: Faker::Internet.url, email: Faker::Internet.email, address: Faker::Address.street_address, status: 'approved', subject_ids: [1,2], theme_ids: [3,4], delivery_ids: [1], age_ids: [1])
-  user.resources.create(title: Faker::Name.unique.name, link: Faker::Internet.url, description: Faker::Company.bs, email: Faker::Internet.email, status: 'pending')
-end
+#10.times do
+#  user.activities.create(title: Faker::Name.title, description: Faker::Company.bs, start_date: '01/13/2019', end_date: '02/13/2019', deadline: '01/13/2019', postcode: Faker::Address.postcode,
+#    link: Faker::Internet.url, email: Faker::Internet.email, address: Faker::Address.street_address, status: 'approved', subject_ids: [1,2], theme_ids: [3,4], delivery_ids: [1], age_ids: [1])
+#  user.resources.create(title: Faker::Name.unique.name, link: Faker::Internet.url, description: Faker::Company.bs, email: Faker::Internet.email, status: 'pending')
+#end
