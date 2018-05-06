1008417892b17d03b48eb22ffa9897ed09cbffae
Isaac <isaacjoyous@outlook.com>
2018-05-06 12:06:35 +0100

    Finished activities + distance controller logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,12 @@ Admin.where(email: 'admin1@sheffield.ac.uk').first_or_create(password:'password'
 
 user = User.find_by_email("fake@sheffield.ac.uk")
 
-#user.activities.create(title: Faker::Name.title, description: Faker::Company.bs, start_date: '01/13/2019', end_date: '02/13/2019', deadline: '01/13/2019', postcode: Faker::Address.postcode, link: Faker::Internet.url, email: Faker::Internet.email, address: Faker::Address.street_address, status: 'approved', theme_ids: [1,2], subject_ids: [1,5], delivery_ids: [1], age_ids: [1])
+user.activities.create(title: Faker::Name.title, description: Faker::Company.bs, start_date: '01/13/2019', end_date: '02/13/2019', deadline: '01/13/2019', postcode: 's12dw', link: Faker::Internet.url, email: Faker::Internet.email, address: Faker::Address.street_address, status: 'approved', theme_ids: [1,2], subject_ids: [1,5], delivery_ids: [1], age_ids: [1])
+user.activities.create(title: Faker::Name.title, description: Faker::Company.bs, start_date: '01/13/2019', end_date: '02/13/2019', deadline: '01/13/2019', postcode: 's37hb', link: Faker::Internet.url, email: Faker::Internet.email, address: Faker::Address.street_address, status: 'approved', theme_ids: [1,2], subject_ids: [1,5], delivery_ids: [1], age_ids: [1])
+user.activities.create(title: Faker::Name.title, description: Faker::Company.bs, start_date: '01/13/2019', end_date: '02/13/2019', deadline: '01/13/2019', postcode: 'gl103qx'  link: Faker::Internet.url, email: Faker::Internet.email, address: Faker::Address.street_address, status: 'approved', theme_ids: [1,2], subject_ids: [1,5], delivery_ids: [1], age_ids: [1])
+
+
+
 #user.activities.create(title: Faker::Name.title, description: Faker::Company.bs, start_date: '01/13/2019', end_date: '02/13/2019', deadline: '01/13/2019', postcode: Faker::Address.postcode, link: Faker::Internet.url, email: Faker::Internet.email, address: Faker::Address.street_address, status: 'approved', theme_ids: [1,3], subject_ids: [3,5], delivery_ids: [2,1], age_ids: [1])
 #user.activities.create(title: Faker::Name.title, description: Faker::Company.bs, start_date: '01/13/2019', end_date: '02/13/2019', deadline: '01/13/2019', postcode: Faker::Address.postcode, link: Faker::Internet.url, email: Faker::Internet.email, address: Faker::Address.street_address, status: 'approved', theme_ids: [2,5], subject_ids: [1], delivery_ids: [], age_ids: [])
 
