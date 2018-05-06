769a4718b5fbc6f25ef55f378b4a5694b7e54af1
owardell-yerburgh1@sheffield.ac.uk <ollie@dyn225246.shef.ac.uk>
2018-05-06 14:22:20 +0100

    added some analytics, answer is now shwon to everyone, vidoes for help

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,32 +4,9 @@ class AdminController < ApplicationController
 
   def index
     @activities = Activity.pending
-    
-  end
-
-  def create
-    @activity = Activity.find(params[:id])
-    @activity.update_column(:status, 'approved')
-    redirect_back(fallback_location: :index)
-  end
-
-  def show
-    @users = User.all
-  end
-
-  def edit
-    @activity = Activity.find(params[:id])
-    puts params[:id]
-    # activity must be linked to user, so that can email them specifically
-
+    @current_nav_identifier = :dashboard
   end
 
 
-  def dash
-    @activities_pending = Activity.pending
-    @activities_approved = Activity.approved
-    @users = User.all
-
-  end
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
 
-
   def home
     @activities = Activity.all.order(cached_votes_up: :desc)
     @subjects = Subject.all.limit(20)
@@ -8,6 +7,16 @@ class PagesController < ApplicationController
   end
 
   def help
+    authorize! :help, @pages
+  end
+
+  def analytics
+    @users = User.all.paginate(page: params[:page], per_page: 14)
+    @learners = User.where(role: 'Learner')
+    @partners = User.where(role: 'Partner')
+    @parents = User.where(role: 'Guardian/Parent')
+    @teachers = User.where(role: 'Teacher/Advisor')
+    authorize! :analytics, @pages
   end
 
 
