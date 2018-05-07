26157ea8d11deed4159b7fb03ff917e216d33179
Isaac <isaacjoyous@outlook.com>
2018-05-07 22:31:06 +0100

    Correctly tested searching a resource with subject, theme + delivery filters applied

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -16,7 +16,6 @@ class SearchesController < ApplicationController
     if params[:postcode] == '' && !current_user
       @distance_filter = false
     end
-
     #filter the activites + resources by query + subject (this uses scopes stored in the relevant models director)
     @activities = Activity.filter(params.slice(:query, :subject, :theme, :delivery)).paginate(page: params[:page], per_page: 10)
     @resources = Resource.filter(params.slice(:query, :subject, :theme, :delivery)).paginate(page: params[:page], per_page: 10)
