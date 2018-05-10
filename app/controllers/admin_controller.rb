4650bc03eb94ac282fb0e354ff6130b35c5412bd
Cesim <fccivisilli1@sheffield.ac.uk>
2018-05-10 16:05:12 +0100

    Most of UI tidy up completed

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,7 +3,7 @@ class AdminController < ApplicationController
   authorize_resource
 
   def index
-    @activities = Activity.pending
+    @activities = Activity.pending.paginate(page: params[:page], per_page: 10)
     @current_nav_identifier = :dashboard
   end
 
