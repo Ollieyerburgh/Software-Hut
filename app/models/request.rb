2e2d823802729dfbcafc07b38c268cb8bf876023
Marilia <melia1@sheffield.ac.uk>
2018-05-16 12:01:01 +0100

    Request.rb comment

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,4 @@
+#This model is used to send the rejectioon email to the user when the admin is not happy with something in the activity submitted by the user
 class Request < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
